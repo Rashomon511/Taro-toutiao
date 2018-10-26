b8c5e218b3c0dac8f463282ec60d1641c9519e3f
rashomon <rashomon511@gmail.com>
2018-10-26 14:20:28 +0800

    taro

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import Taro, { Component } from '@tarojs/taro'
+import Index from './pages/index'
+
+import './app.less'
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentCatchError () {}
+
+  render () {
+    return (
+      <Index />
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
